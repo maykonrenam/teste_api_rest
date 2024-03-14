a1fe56a672b406d4f5eb84333e5ef057e9b0a58a
Maykon Melo <maykon.rsm@gmail.com>
2024-03-14 10:01:12 -0300

    Testar rotas

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 import http from 'http';
 
 const PORT = 3000;
+const rotas = {
+    "/": "Rota da página inicial",
+    "/livros": "Rota da página de livros2",
+    "/autores": "Rota da página de autores"
+};
 
 // Cria um servidor HTTP que responde com um texto simples
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World');
+    res.end(rotas[req.url]);
 });
 
 // O servidor local recebe conexões na porta 3000
